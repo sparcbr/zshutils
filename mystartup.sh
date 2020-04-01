0a307b4cbaa356d7afc0b103574f32d042f09010
Elizandro G. Roos <sparcbr@gmail.com>
2020-04-01 19:12:16 -0300

    Saving everything

--- mystartup.sh
+++ mystartup.sh
@@ -1,17 +1,26 @@
 #!/bin/zsh
 #notify-send -t 15000 "Starting $0 apps"
-cd `dirname $0`
-xhost +
-#$HOME/bin/set_def_sound_source.sh
-ssh-add ~/.ssh/id_rsa.1
-setopt extendedglob
-if [[ "$(lspci | grep VGA)" != (#i)*nvidia*(#q) ]]; then
-	include proc
-	rpid=$(pidof radeon-profile) || {
-		$HOME/xdata/radeon-profile/radeon-profile/radeon-profile &
-		rpid=$!
-		xdotool search --all --sync --onlyvisible --pid $rpid --name 'Radeon Profile'  mousemove --sync --window '%1' 20 265 click 1 key Down key Return mousemove --sync --window '%1' 20 170 click 1 #windowminimize
-	}
+cd $0:P:h
+if [[ $1 == 'post' ]]; then
+	notify-send 'Resuming'
+	sleep 2
+	if ! ping -i 0.2  -w 2 -W 0.1 -c 2 -rn 192.168.1.1; then
+		notify-send 'Restarting networking'
+		sudo service networking restart
+	fi
+else
+	xhost +
+	#$HOME/bin/set_def_sound_source.sh
+	ssh-add ~/.ssh/id_rsa.1
+	setopt extendedglob
+	#if [[ "$(lspci | grep VGA)" != (#i)*nvidia*(#q) ]]; then
+	#	include proc
+	#	rpid=$(pidof radeon-profile) || {
+	#		$HOME/xdata/radeon-profile/radeon-profile/radeon-profile &
+	#		rpid=$!
+	#		xdotool search --all --sync --onlyvisible --pid $rpid --name 'Radeon Profile'  mousemove --sync --window '%1' 20 265 click 1 key Down key Return mousemove --sync --window '%1' 20 170 click 1 #windowminimize
+	#	}
+	#fi
+	sudo cpuconfig &
+	redshift-gtk&
 fi
-sudo cpuconfig
-redshift-gtk&
